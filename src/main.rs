c808f756218cfd4cd30835a7dd7d9ad1b126621d
john <jhopkin@protonmail.com>
2023-01-17 21:39:16 +1000

    update the serve address

--- src/main.rs
+++ src/main.rs
@@ -45,5 +45,5 @@ async fn main() {
         // .with(cors)
         // .recover(return_error);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8088)).await;
 }
